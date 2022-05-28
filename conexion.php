085178cabb2cfe6b365e8311ccff9d909d885f75
Mariana Forlenza <85969334+marianaforlenza@users.noreply.github.com>
2022-05-28 14:32:31 -0300

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,12 @@
+
+<?php
+
+
+
+$servidorBD="fdb34.awardspace.net";
+$usuarioBD="3988599_productos";
+$contraBD="vjm123456";
+$baseDatosBD="3988599_productos";
+
+
+?>
---
+++ loggin.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+
+
+
+//traer datos desde el formulario del navegador
+$usuarioForm=$_POST['usuario'];
+$passForm=$_POST['pass'];
+
+//echo "usuario $usuarioForm, pass $passForm";
+
+require "conexion.php";
+
+
+$con=mysqli_connect($servidorBD, $usuarioBD,$contraBD,$baseDatosBD) or die ("no se puede conectar a la base de datos");
+
+
+$sqlVerifica="select * from usu where usuario='$usuarioForm';";
+
+$resulset=mysqli_query($con, $sqlVerifica);
+
+$registro=mysqli_fetch_assoc($resulset);
+
+
+
+if(mysqli_affected_rows($con)>0){
+    //echo "Se encontró el usuario";
+
+    $usu=$registro['usuario'];
+    $contra= $registro['contra'];
+    $nomyape= $registro['nombre']." ".$registro['apellido'];
+    $id= $registro['id_usuario'];
+    //verifico pass
+    if($registro['contra']==$passForm){
+        session_start();
+        //echo "<br><br><br><h3> Inicia sesión <h3>";
+        //cargar variables de sesion
+        $_SESSION['id_usuario']=$id;
+        $_SESSION['usuario']=$usu;
+        $_SESSION['nombre']=$nomyape;  
+        ?>
+    <meta http-equiv="Refresh" content="2; url=menu.php">
+
+<?php
+
+    }
+    else{
+        echo "<br><br><br><h3> La contraseña es incorrecta <h3>";
+        echo '<meta http-equiv="Refresh" content="1; url=index.php">';
+    }
+    
+?>
+
+
+<?php
+
+}
+else{
+    echo "<br><br><br><h3> No existe el usuario $usuarioForm <h3>";
+    echo '<meta http-equiv="Refresh" content="1; url=index.php">';
+}
+
+?>
