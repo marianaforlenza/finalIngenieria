4fa077cc3edc0d2ae44f90c022b7e0ae9b4f7c1e
Jairpsa <87381885+Jairpsa@users.noreply.github.com>
2022-06-05 07:55:46 -0300

    Add files via upload

--- guardarCambios.php
+++ guardarCambios.php
@@ -1,4 +1,15 @@
-
+<?php
+session_start();
+if(isset($_SESSION['usu'])){
+    $nomyape= $_SESSION['nombre'];
+    $nroUsuario = $_SESSION['nroUsuario'];
+}
+else{
+    echo "ACCESO NO AUTORIZADO<br> DEBE INICIAR SESIÓN";
+    echo '<meta http-equiv="Refresh" content="3; url=index.php">';
+    exit();
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,29 +30,64 @@ $nombre = $_POST['nombreB'];
 $precio = $_POST['precioB'];
 $stock = $_POST['stockB'];
 $cat = $_POST['categoriaB'];
-if(isset($_POST['sumarStock'])){
-    $sumarStock = $_POST['sumarStock'];
-}else{
-    $sumarStock = 0;
-}
-if(isset($_POST['restarStock'])){
-    $restarStock = $_POST['restarStock'];
-}else{
-    $restarStock = 0;
-}
+$marca = $_POST['marcaB'];
+$sumarStock = $_POST['sumarStock'];
+$restarStock = $_POST['restarStock'];
+
+$hoy= date('Y-m-d');
+
+
 echo "sumar stock es = $sumarStock, y restar stock es = $restarStock";
 $total = $sumarStock - $restarStock;
 $stockNuevo = $stock + $total;
 
 $conne = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die("no se pudo conectar a la BD");
 
+// update Productos
 $sqlGuardarCambios = "update productos set detalle = '$nombre', precio = $precio,
-stock = $stockNuevo, TIPO_PRODUCTOS_idTIPO_PRODUCTOS = $cat 
-where CodProducto = $codProd";
+                      stock = $stockNuevo, TIPO_PRODUCTOS_idTIPO_PRODUCTOS = $cat 
+                      where CodProducto = $codProd";
 
 $result = mysqli_query($conne, $sqlGuardarCambios);
 
-// $sqlGuardarCambiosEntradas
+// guardar datos en Entradas
+if($sumarStock != 0){
+    $connex = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die ("no se pudo conectar a la Base de datos");
+    $sqlGuardarEntr = "insert into entradas (cantidadEntrada, fechaEntrada, USUARIOS_nroUsuario)
+                       values ($sumarStock, '$hoy', $nroUsuario);";
+    $resulset3 = mysqli_query($connex,$sqlGuardarEntr);
+
+// insert Entradas_has_productos
+    $id_autoincr_Entr = mysqli_insert_id($connex);
+
+    $sqlEntr_Prod = "insert into entradas_has_productos (ENTRADAS_nroEntrada, PRODUCTOS_CodProducto_Entr, CantidadEntr)
+                     values ($id_autoincr_Entr, $codProd, $sumarStock);";
+    $resulset4 = mysqli_query($connex, $sqlEntr_Prod);
+}
+
+// insert Salidas
+if($restarStock != 0){
+    $conexi = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die ("no se pudo conectar a la Base de datos");
+    $sqlGuardarSal = "insert into salidas (cantidadSalida, FechaSalida, USUARIOS_nroUsuarioSal)
+                      values ($restarStock, '$hoy', $nroUsuario);";
+    $resulset5 = mysqli_query ($conexi, $sqlGuardarSal);
+
+// insert Productos_has_salidas
+    $id_autoincr_Sal = mysqli_insert_id($conexi);
+
+    $sqlSal_Prod = "insert into productos_has_salidas (SALIDAS_nroSalida, PRODUCTOS_CodProducto_Sal, cantidadSal)
+                    values ($id_autoincr_Sal, $codProd, $restarStock);";
+    $resulset6 = mysqli_query($conexi, $sqlSal_Prod);
+}
+
+// update Productos_has_Marcas
+$sqlGuardarMarca = "update productos_has_marcas set MARCAS_nroMarca = $marca
+                    where PRODUCTOS_CodProducto = $codProd";
+$result7 = mysqli_query($conne, $sqlGuardarMarca);
+
+
+
+
 
 echo '<meta http-equiv="Refresh" content="0; url=listarProd.php"'
 
--- modificar.php
+++ modificar.php
@@ -15,10 +15,11 @@ $prodModificar = $_POST['selec'];
 
 require "conexion.php";
     $con=mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die("no se pudo conectar a la BD");
-
     $sqlModificarProd="select * from productos where CodProducto='$prodModificar'";
-
     $resultProdModificar =mysqli_query($con, $sqlModificarProd);
+
+
+
 ?>
 
 <form action=guardarCambios.php method=post>
@@ -28,20 +29,86 @@ require "conexion.php";
     <th>Nombre</th>
     <th>Precio</th>
     <th>Stock actual</th>
-    <th>Categoría</th> 
+    <th>Categoría</th>
+    <th>Marca</th> 
     </tr>
 <?php
 while($fila=mysqli_fetch_row($resultProdModificar)){
     ?><tr>
-    <td><input type=text readonly name=codProd value='<?php echo $fila[0]?>'></td>
-    <!-- <input type=hidden name=codigoB value='<?php echo $fila[0]?>'> -->
-    <td><input type=text name=nombreB value='<?php echo $fila[1]?>'></td>
-    <td><input type=number name=precioB value='<?php echo $fila[2]?>'></td>
-    <td><input type=number readonly name=stockB value='<?php echo $fila[3]?>'></td>
-    <td><input type=text name=categoriaB value='<?php echo $fila[4]?>'></td>
-    </tr><br><br>
+    <td><input type=text readonly name=codProd value='<?php echo $fila[0]?>'></td>  <!-- Columna Código Producto -->
+    <td><input type=text name=nombreB value='<?php echo $fila[1]?>'></td>   <!-- Columna Nombre -->
+    <td><input type=number name=precioB value='<?php echo $fila[2]?>'></td> <!-- Columna Precio -->
+    <td><input type=number readonly name=stockB value='<?php echo $fila[3]?>'></td> <!-- Columna Stock -->
+    <!-- Columna Categoría -->
+    <?php
+    $sqlBuscar_Cat = "select * from tipo_productos where idTIPO_PRODUCTOS = '$fila[4]'";
+    $recordset_Cat = mysqli_query($con, $sqlBuscar_Cat);
+    $registro_Cat = mysqli_fetch_row($recordset_Cat);
+    $id_Cat = $registro_Cat[0];
+    $detalle_Cat = $registro_Cat[1];
+    ?>
+
+    <td><select name="categoriaB">
+    <?php
+    $getTipoProd1 = "select * from tipo_productos";
+    $getTipoProd2 = mysqli_query($con, $getTipoProd1);
+    while($row1 = mysqli_fetch_array($getTipoProd2))
+    {
+        $idTIPO_PRODUCTOS = $row1['idTIPO_PRODUCTOS'];
+        $descripcion1 = $row1['descripcion'];
+        if($idTIPO_PRODUCTOS == $id_Cat){
+            $selected = "selected";
+        }else{
+            $selected = "";
+        }
+    ?>
+    <option value="<?php echo $idTIPO_PRODUCTOS;?>" <?php echo $selected ?>> <?php echo $descripcion1 ?> </option>
+    <?php
+    }
+    ?>
+</select>
+</td>
+
+<!-- Columna Marca -->
+    <?php
+    $sqlBuscar_Marca = "select * from productos_has_marcas where PRODUCTOS_CodProducto = '$fila[0]'";
+    $recordset_Marca = mysqli_query($con, $sqlBuscar_Marca);
+    $registro_Marca = mysqli_fetch_row($recordset_Marca);
+    $sqlBuscar_Marca2 = "select * from marcas where nroMarca = '$registro_Marca[1]'";
+    $recordset_Marca2 = mysqli_query ($con, $sqlBuscar_Marca2);
+    $registro_Marca2 = mysqli_fetch_row($recordset_Marca2);
+    $idMarca = $registro_Marca2[0];
+    $detalle_Marca = $registro_Marca2[1];
+    ?>
+
+    <td><select name="marcaB">
+    <?php
+    $getMarcas1 = "select * from marcas";
+    $getMarcas2 = mysqli_query($con, $getMarcas1);
+    while($row = mysqli_fetch_array($getMarcas2))
+    {
+      $nroMarca = $row['nroMarca'];
+      $descripcion = $row['descripcion'];
+      if($nroMarca == $idMarca){
+        $selected = "selected";
+    }else{
+        $selected = "";
+    }
+    ?>
+    <option value="<?php echo $nroMarca; ?>" <?php echo $selected ?>> <?php echo $descripcion ?> </option>
+    <?php
+    }
+    ?>
+    </select>
+    </td>
+
+
+
+</tr><br><br>
 </table><br>
 
+<br>
+<br>
     Sumar stock<input type=number name=sumarStock value=0 >
     Restar stock<input type=number name=restarStock value=0><br>
 
@@ -49,15 +116,12 @@ while($fila=mysqli_fetch_row($resultProdModificar)){
     <input type=submit value="Guardar cambios">
     </div><br>
 
+
+
 </form>
 <?php
 }
 ?>
-
-
-
-
-
 
 
 </body>
