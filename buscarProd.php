0cb59a27638947b7b8a1b227a1d15562e782c0ec
Mariana Forlenza <85969334+marianaforlenza@users.noreply.github.com>
2022-06-05 21:22:04 -0300

    Update buscarProd.php

--- buscarProd.php
+++ buscarProd.php
@@ -1,4 +1,28 @@
 
+<?php
+session_start();
+if(isset($_SESSION['usu'])){
+
+$usu=$_SESSION['usu'];
+ $nomyape=$_SESSION['nombre'];  
+
+
+
+}
+
+else{
+
+echo "Acceso No Autorizado. Debe iniciar Sesion";
+echo '<meta http-equiv-"Refresh" content="2; url=index.php>';
+//echo $usu;
+//echo "el nombre es" .$nombre;
+exit();
+}
+
+
+?>
+
+
 
 
 <!DOCTYPE html>
