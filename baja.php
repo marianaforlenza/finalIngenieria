62c4e07cd4eb41be540d07286e4c114340c25829
silvarvalerias <106923232+silvarvalerias@users.noreply.github.com>
2022-06-08 17:38:33 -0300

    Add files via upload

--- baja.php
+++ baja.php
@@ -1,17 +1,3 @@
-<?php
-session_start();
-if(isset($_SESSION['usu'])){
-    $nomyape= $_SESSION['nombre'];
-    $nroUsuario = $_SESSION['nroUsuario'];
-}
-else{
-    echo "ACCESO NO AUTORIZADO<br> DEBE INICIAR SESIÓN";
-    echo '<meta http-equiv="Refresh" content="3; url=index.php">';
-    exit();
-}
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -62,7 +48,28 @@ else{
           $resulset3 = mysqli_query($con2,$sqlEliminar3);
           
           if (mysqli_affected_rows ($con2)>0){
-              echo '<font color="white">La entrada se ha eliminado correctamente.</font><br><br>';
+            //echo '<font color="white">La entrada se ha eliminado correctamente.</font><br><br>'; 
+            // ELIMINO LA RELACION PRODUCTO - MARCA
+            $con4 = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die ("no se pudo conectar a la Base de datos");
+            $sqlEliminar4="delete from productos_has_marcas where PRODUCTOS_CodProducto = $CodProducto";
+            $resulset4 = mysqli_query($con4,$sqlEliminar4);
+            if (mysqli_affected_rows ($con4)>0){
+              //echo '<font color="white">El Producto se ha eliminado correctamente.</font><br><br>';
+              // ELIMINO EL PRODUCTO
+              $con5 = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die ("no se pudo conectar a la Base de datos");
+              $sqlEliminar5="delete from productos where CodProducto = $CodProducto";
+              $resulset5 = mysqli_query($con5,$sqlEliminar5);
+              if (mysqli_affected_rows ($con5)>0){
+                  echo '<font color="white">El Producto se ha eliminado correctamente.</font><br><br>';
+               }
+              else{
+                  echo '<font color="white">No se pudo eliminar el producto: '.$detalle.'.</font><br><br>';
+               }
+         
+            }
+            else{
+                echo '<font color="white">No se pudo eliminar la relacion prod-marca.</font><br><br>';
+            }
           }
           else{
               echo '<font color="white">No se pudo eliminar la entrada: '.$nroentrada.'.</font><br><br>';
@@ -76,24 +83,24 @@ else{
   };
   
      // ELIMINO LA RELACION PRODUCTO - MARCA
-  $con4 = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die ("no se pudo conectar a la Base de datos");
-  $sqlEliminar4="delete from productos_has_marcas where PRODUCTOS_CodProducto = $CodProducto";
-  $resulset4 = mysqli_query($con4,$sqlEliminar4);
+  //$con4 = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die ("no se pudo conectar a la Base de datos");
+  //$sqlEliminar4="delete from productos_has_marcas where PRODUCTOS_CodProducto = $CodProducto";
+  //$resulset4 = mysqli_query($con4,$sqlEliminar4);
 
   // ELIMINO EL PRODUCTO
-  $con5 = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die ("no se pudo conectar a la Base de datos");
-  $sqlEliminar5="delete from productos where CodProducto = $CodProducto";
-  $resulset5 = mysqli_query($con5,$sqlEliminar5);
+  //$con5 = mysqli_connect($servidorBD, $usuarioBD, $contraBD, $baseDatosBD) or die ("no se pudo conectar a la Base de datos");
+  //$sqlEliminar5="delete from productos where CodProducto = $CodProducto";
+  //$resulset5 = mysqli_query($con5,$sqlEliminar5);
 
-  if (mysqli_affected_rows ($con5)>0){
-      echo '<font color="white">El Producto se ha eliminado correctamente.</font><br><br>';
-  }
-  else{
-      echo '<font color="white">No se pudo eliminar el producto: '.$detalle.'.</font><br><br>';
-  }
+  //if (mysqli_affected_rows ($con5)>0){
+  //    echo '<font color="white">El Producto se ha eliminado correctamente.</font><br><br>';
+  //}
+  //else{
+  //  echo '<font color="white">No se pudo eliminar el producto: '.$detalle.'.</font><br><br>';
+  //}
  
   ?>
-  <a href=listarProd.php><input type=button value="Volver"></a> <br><br><br> </form> 
+  <a href=listarprod.php><input type=button value="Volver"></a> <br><br><br> </form> 
   <?php
 
 
